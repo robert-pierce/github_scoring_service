e74cc236097028e248e59fde67c11bb8d071712b
Robert Pierce <robert.pierce@careerbuilder.com>
2018-03-10 19:53:13 -0500

    check db connection on startup now

--- src/github_scoring_service/init.clj
+++ src/github_scoring_service/init.clj
@@ -1,10 +1,28 @@
 (ns github-scoring-service.init
   (:require [clojure.string :as string]
             [clj-log.core :refer :all]
+            [github-scoring-service.db :as db]
             [github-scoring-service.config :as c]))
+
+(defn dev-env-info
+  []
+  (log :info (str "Database Username: " (:db-username c/config)))
+  (log :info (str "Database Url: " (:db-url c/config))))
+
+(defn check-db
+  []
+  (try 
+    (let [test-query-result (db/show-tables)]
+      (if (> (count test-query-result) 0)
+        (log :info (str "The app is connected to a database with at least one table"))
+        (if (= (count test-query-result) 0)
+          (log :warn (str "The app seems to be connected to a database, but there are no tables present"))
+          (log :warn (str "The app does not appear to be connected to a database")))))
+   (catch Exception e
+     (log :warn (str "Not able to connect to database. Exception:  " (.getMessage e))))))
 
 (defn init-app
   []
   (log :info "Initializing Github_Scoring_Service")
-  (log :info (str "Database Username: " (:db-username c/config)))
-  (log :info (str "Database Url: " (:db-url c/config))))
+  (dev-env-info)
+  (check-db))
