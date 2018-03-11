b5ab44221b5a5faa62114c9558666fd21b722964
Robert Pierce <robert.pierce@careerbuilder.com>
2018-03-10 22:17:44 -0500

    updated event processor

--- src/github_scoring_service/event_processor.clj
+++ src/github_scoring_service/event_processor.clj
@@ -1,7 +1,34 @@
 (ns github-scoring-service.event_processor
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clj-log.core :refer :all]
+            [github-scoring-service.config :as c]
+            [github-scoring-service.db :as db]))
+
+(def value-map {"push" (Integer/parseInt (:push-event-value c/config)) 
+                "pull_request_review_comment" (Integer/parseInt (:pr-comment-value c/config)) 
+                "watch" (Integer/parseInt (:watch-event-value c/config)) 
+                "create" (Integer/parseInt (:create-event-value c/config))})
+
+(defn get-event-point-value
+  [event-type]
+  (if-let [value (get value-map event-type)]
+    value
+    1))
 
 (defn process-event
   [request]
-  (println request)
-  (:body request))
+  (let [headers (:headers request)
+        body (:body request)
+        event-type (get headers "x-github-event")
+        guid (get headers "x-github-delivery")
+        github-user (get-in body ["sender" "login"])
+        repository (get-in body ["repository" "full_name"])
+        event-value (get-event-point-value event-type)]
+    (try
+      (let [db-response 
+            (db/persist-event! guid event-type event-value github-user repository)]
+        (println db-response)
+        true)
+      (catch Exception e
+        (log :error (str "Error saving event in the databae. Exception: " (.getMessage e)))
+        false))))
